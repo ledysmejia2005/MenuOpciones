ddb9eedd1887abd3d7827d87f8c111fed4897fb6
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-09-09 14:19:13 -0500

    creando ciclos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        //Programa para crear un ciclo
+        // y crear un menu de opciones
+        Integer opcionSeleccionada=0;
+        while(opcionSeleccionada != 5){
+            System.out.println("Menu de opciones");
+            System.out.println("****************+*");
+            System.out.println("➕Digita uno para sumar");
+            System.out.println("➖Digita 2 para restar");
+            System.out.println("✖ Digita 3 para multiplicar");
+            System.out.println("➗Digita 4 para dividir");
+            System.out.println("🏃‍♀️Digita 5 para SALIR DEL PROGRAMA");
+            Scanner leerTeclado= new Scanner(System.in);
+            System.out.println("Selecciona una opción por favor:😊");
+            opcionSeleccionada=leerTeclado.nextInt();
+        }
     }
 
 }
